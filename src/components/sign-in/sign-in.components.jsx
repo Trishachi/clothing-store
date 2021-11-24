4959dba7d92b727d09cadf333cf3e49da4bbeafa
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2021-11-23 23:29:46 -0700

    Firebase Sign In and SignOut User Auth Completed

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -33,9 +33,11 @@ class SignIn extends React.Component{
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="email" required  />                    
-                    <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required />                    
+                    <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required />    
+                    <div className="buttons">                
                     <CustomButton type="submit" value="Submit Form">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} >{' '} Sign in with Google {' '}</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>{' '} Sign in with Google {' '}</CustomButton>
+                    </div>
                 </form>
             </div>
         )
